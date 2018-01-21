76fdd7b34de8877088f11960e553d16fbd92268b
Ilian Sapundshiev <ilian@tagspaces.org>
2018-01-21 15:44:33 +0100

    adding animation while scrapping saving dataurls as jpg at 0.9 compression refactoring the imgs scrapping adding meta data by scrapping the whole page

--- src/ui/popup.js
+++ src/ui/popup.js
@@ -21,10 +21,12 @@ $(document).ready(init);
 
 console.log('Loading Popup...');
 
-const supportedExts = ['png', 'jpg', 'jpeg', 'svg', 'webp', 'gif', 'pdf', 'ogg', 'ogv', 'mp4', 'mp3'];
+const isWin = navigator.appVersion.includes('Win');
+const dirSeparator = isWin ? '\\' : '/';
 const isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
 const isChrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
-let htmlTemplate = '<!DOCTYPE html><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf - 8"><style type="text/css">body{overflow:auto;width:100%;height:100%;font:13.34px Ubuntu,arial,clean,sans-serif;color:#000;line-height:1.4em;background-color:#fff;padding:15px}p{margin:1em 0;line-height:1.5em}table{font:100%;margin:1em}table th{border-bottom:1px solid #bbb;padding:.2em 1em}table td{border-bottom:1px solid #ddd;padding:.2em 1em}input[type=image],input[type=password],input[type=text],textarea{font:99% helvetica,arial,freesans,sans-serif}option,select{padding:0 .25em}optgroup{margin-top:.5em}code,pre{font:12px Monaco, Courier ,monospace}pre{margin:1em 0;font-size:12px;background-color:#eee;border:1px solid #ddd;padding:5px;line-height:1.5em;color:#444;overflow:auto;-webkit-box-shadow:rgba(0,0,0,.07) 0 1px 2px inset;-webkit-border-radius:3px;-moz-border-radius:3px;border-radius:3px}pre code{padding:0;font-size:12px;background-color:#eee;border:none}code{font-size:12px;background-color:#f8f8ff;color:#444;padding:0 .2em;border:1px solid #dedede}img{border:0;max-width:100%}abbr{border-bottom:none}a{color:#4183c4;text-decoration:none}a:hover{text-decoration:underline}a code,a:link code,a:visited code{color:#4183c4}h2,h3{margin:1em 0}h1,h2,h3,h4,h5,h6{border:0}h1{font-size:170%;border-top:4px solid #aaa;padding-top:.5em;margin-top:1.5em}h1:first-child{margin-top:0;padding-top:.25em;border-top:none}h2{font-size:150%;margin-top:1.5em;border-top:4px solid #e0e0e0;padding-top:.5em}h3{font-size:130%;margin-top:1em}h4{font-size:120%;margin-top:1em}h5{font-size:115%;margin-top:1em}h6{font-size:110%;margin-top:1em}hr{border:1px solid #ddd}ol,ul{margin:1em 0 1em 2em}ol li,ul li{margin-top:.5em;margin-bottom:.5em}ol ol,ol ul,ul ol,ul ul{margin-top:0;margin-bottom:0}blockquote{margin:1em 0;border-left:5px solid #ddd;padding-left:.6em;color:#555}dt{font-weight:700;margin-left:1em}dd{margin-left:2em;margin-bottom:1em}</style></head><body></body></html>';
+const supportedExts = ['png', 'jpg', 'jpeg', 'svg', 'webp', 'gif', 'pdf', 'ogg', 'ogv', 'mp4', 'mp3'];
+let htmlTemplate = '<!DOCTYPE html><html><head><meta charset="UTF-8"><style type="text/css">body{overflow:auto;width:100%;height:100%;font:13.34px Ubuntu,arial,clean,sans-serif;color:#000;line-height:1.4em;background-color:#fff;padding:15px}p{margin:1em 0;line-height:1.5em}table{font:100%;margin:1em}table th{border-bottom:1px solid #bbb;padding:.2em 1em}table td{border-bottom:1px solid #ddd;padding:.2em 1em}input[type=image],input[type=password],input[type=text],textarea{font:99% helvetica,arial,freesans,sans-serif}option,select{padding:0 .25em}optgroup{margin-top:.5em}code,pre{font:12px Monaco, Courier ,monospace}pre{margin:1em 0;font-size:12px;background-color:#eee;border:1px solid #ddd;padding:5px;line-height:1.5em;color:#444;overflow:auto;-webkit-box-shadow:rgba(0,0,0,.07) 0 1px 2px inset;-webkit-border-radius:3px;-moz-border-radius:3px;border-radius:3px}pre code{padding:0;font-size:12px;background-color:#eee;border:none}code{font-size:12px;background-color:#f8f8ff;color:#444;padding:0 .2em;border:1px solid #dedede}img{border:0;max-width:100%}abbr{border-bottom:none}a{color:#4183c4;text-decoration:none}a:hover{text-decoration:underline}a code,a:link code,a:visited code{color:#4183c4}h2,h3{margin:1em 0}h1,h2,h3,h4,h5,h6{border:0}h1{font-size:170%;border-top:4px solid #aaa;padding-top:.5em;margin-top:1.5em}h1:first-child{margin-top:0;padding-top:.25em;border-top:none}h2{font-size:150%;margin-top:1.5em;border-top:4px solid #e0e0e0;padding-top:.5em}h3{font-size:130%;margin-top:1em}h4{font-size:120%;margin-top:1em}h5{font-size:115%;margin-top:1em}h6{font-size:110%;margin-top:1em}hr{border:1px solid #ddd}ol,ul{margin:1em 0 1em 2em}ol li,ul li{margin-top:.5em;margin-bottom:.5em}ol ol,ol ul,ul ol,ul ul{margin-top:0;margin-bottom:0}blockquote{margin:1em 0;border-left:5px solid #ddd;padding-left:.6em;color:#555}dt{font-weight:700;margin-left:1em}dd{margin-left:2em;margin-bottom:1em}</style></head><body></body></html>';
 let fileExt;
 let currentTabURL;
 let currentTabID;
@@ -43,7 +45,7 @@ function init() {
 
   $('#title').focus();
   isFirefox ? $("#saveAsMhtml").hide() : $("#saveAsMhtml").on('click', saveAsMHTML);
-  isChrome ? $("#saveWholePageAsHtml").hide() : '';
+  // isChrome ? $("#saveWholePageAsHtml").hide() : '';
   $("#closePopup").on('click', () => window.close());
   $("#saveAsBookmark").on('click', saveAsBookmark);
   $("#saveSelectionAsHtml").on("click", saveSelectionAsHTML);
@@ -61,14 +63,6 @@ function init() {
   browser.runtime.onMessage.addListener(handleHTMLContent);
 }
 
-function saveAsBookmark() {
-  const content = '[InternetShortcut]\r\nURL=' + currentTabURL + '';
-  const textBlob = new Blob([content], {
-    type: "text/plain;charset=utf-8"
-  });
-  saveAs(textBlob, generateFileName('url'));
-}
-
 function handleHTMLContent (request) {
   if (request.action == "htmlcontent") {
     //console.log("HTML: " + request.source);
@@ -83,25 +77,8 @@ function handleHTMLContent (request) {
   }
 }
 
-function saveSelectionAsHTML() {
-  const executing = browser.tabs.executeScript(null, {
-    file: "content-script-capture-selection.dist.js"
-  });
-  executing.then(() => {
-    console.log('Content script injected...');
-  }, (err) => console.warn('Error executing script ' + JSON.stringify(err)));
-}
-
-function saveWholePageAsHTML() {
-  const executing = browser.tabs.executeScript(null, {
-    file: "content-script-capture-wholepage.dist.js"
-  });
-  executing.then(() => {
-    console.log('Content script injected...');
-  }, (err) => console.warn('Error executing script ' + JSON.stringify(err)));
-}
-
 function saveAsMHTML() {
+  $('#saveAsMhtml i').removeClass('fa-file-image-o').addClass('fa-spin fa-circle-o-notch');
   if (fileExt === 'mhtml') {
     browser.pageCapture.saveAsMHTML({
       tabId: currentTabID
@@ -117,7 +94,28 @@ function saveAsMHTML() {
   }
 }
 
+function saveWholePageAsHTML() {
+  $('#saveWholePageAsHtml i').removeClass('fa-file').addClass('fa-spin fa-circle-o-notch');
+  const executing = browser.tabs.executeScript(null, {
+    file: "content-script-capture-wholepage.dist.js"
+  });
+  executing.then(() => {
+    console.log('Content script injected...');
+  }, (err) => console.warn('Error executing script ' + JSON.stringify(err)));
+}
+
+function saveSelectionAsHTML() {
+  $('#saveSelectionAsHtml i').removeClass('fa-file-text').addClass('fa-spin fa-circle-o-notch');
+  const executing = browser.tabs.executeScript(null, {
+    file: "content-script-capture-selection.dist.js"
+  });
+  executing.then(() => {
+    console.log('Content script injected...');
+  }, (err) => console.warn('Error executing script ' + JSON.stringify(err)));
+}
+
 function saveScreenshot() {
+  $('#saveScreenshot i').removeClass('fa-camera').addClass('fa-spin fa-circle-o-notch');
   const capturing = browser.tabs.captureVisibleTab(null, {
     "format": "png"
   });
@@ -126,14 +124,28 @@ function saveScreenshot() {
   }, (err) => console.warn('Error taking screenshot ' + JSON.stringify(err)));
 }
 
+function saveAsBookmark() {
+  $('#saveAsBookmark i').removeClass('fa-bookmark').addClass('fa-spin fa-circle-o-notch');
+  const content = '[InternetShortcut]\r\nURL=' + currentTabURL + '';
+  const textBlob = new Blob([content], {
+    type: "text/plain;charset=utf-8"
+  });
+  saveAs(textBlob, generateFileName('url'));
+}
+
 function generateFileName(extension) {
-  let filename;
+  let filename = $('#title').val()
+  const lastIndexOfDot = filename.lastIndexOf('.');
+  // removing the extension if the dot in for 4 or less character before teh end of the title
+  if (lastIndexOfDot > 0 && (filename.length - lastIndexOfDot) < 5) {
+    filename = filename.substring(0, filename.lastIndexOf('.'))
+  }
   let tags = document.getElementById("tags").value;
   if (tags) {
     tags = tags.split(",").join(" ");
-    filename = $('#title').val() + ' [' + tags + '].' + extension;
+    filename = filename + ' [' + tags + '].' + extension;
   } else {
-    filename = $('#title').val() + '.' + extension;
+    filename = filename + '.' + extension;
   }
   return filename;
 }
@@ -172,7 +184,7 @@ function getBase64ImagePromise(imgURL) {
       canvas.width = img.width;
       canvas.height = img.height;
       ctx.drawImage(img, 0, 0);
-      dataURL = canvas.toDataURL("image/png");
+      dataURL = canvas.toDataURL('image/jpeg', 0.9);
       resolve([imgURL, dataURL]);
     }
   });
@@ -185,28 +197,41 @@ function getFileExt(fileName) {
 
 function prepareContentPromise(uncleanedHTML) {
   return new Promise((resolve) => {
-    //console.log("uncleaned "+uncleanedHTML);
+    // console.log("uncleaned "+uncleanedHTML);
+    console.log("prepareContentPromise for tab with url " + currentTabURL);
     var cleanedHTML = uncleanedHTML.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, "");
     // var cleanedHTML = DOMPurify.sanitize(uncleanedHTML);
-
     // console.log("cleaned "+cleanedHTML);
 
-    // saving all images as png in base64 format
+    // saving all images as jpg in base64 format
     let match;
     const urlPromises = [];
-    let imgUrl = '';
     let originalImgUrl;
-    const rex = /<img.*?src="([^">]*\/([^">]*?))".*?>/g;
+    let baseTabPath;
+    const rex = /<img.*?src=['"](.*?)['"]/g;
+    const imgSources = [];
 
-    while ( match = rex.exec( cleanedHTML ) ) {
-      imgUrl = match[1];
-      if (!imgUrl.startsWith('http')) {
+    while ((match = rex.exec(cleanedHTML)) !== null) {
+      imgSources.push(match[1]);
+      // console.log(`Found ${match[1]} in ${match[0]}`);
+    }
+    for (let imgUrl of imgSources) {
+      if (imgUrl.startsWith('data')) {
+        // ignoring data urls
+      } else if (imgUrl.startsWith('http') || imgUrl.startsWith('file')) {
+        urlPromises.push(getBase64ImagePromise(imgUrl));
+      } else {
         originalImgUrl = imgUrl;
-        imgUrl = currentTabURL + imgUrl;
+        if (currentTabURL.startsWith('file:')) {
+          baseTabPath = currentTabURL.substring(0, currentTabURL.lastIndexOf(dirSeparator) + 1);
+          imgUrl = baseTabPath + imgUrl;
+        } else {
+          imgUrl = currentTabURL + imgUrl;
+        }
         cleanedHTML = cleanedHTML.split(originalImgUrl).join(imgUrl);
+        urlPromises.push(getBase64ImagePromise(imgUrl));
       }
-      // console.log("URLs: "+imgUrl);
-      urlPromises.push(getBase64ImagePromise(imgUrl));
+      console.log("URLs: "+imgUrl);
     }
 
     Promise.all(urlPromises).then((resultUrls) => {
@@ -215,9 +240,12 @@ function prepareContentPromise(uncleanedHTML) {
         //console.log(dataURLObject[0]+" - "+dataURLObject[1]);
       });
 
-      if (!cleanedHTML.includes('<body')) {
-        cleanedHTML = "<body data-sourceurl='" + currentTabURL + "' data-scrappedon='" + (new Date()).toISOString() + "' >" + cleanedHTML + "</body>";
-        cleanedHTML = htmlTemplate.replace(/\<body[^>]*\>([^]*)\<\/body>/m, cleanedHTML); // jshint ignore:line
+      const metaData = 'data-sourceurl="' + currentTabURL + '" data-scrappedon="' + (new Date()).toISOString() + '"';
+      if (cleanedHTML.includes('<body')) {
+        cleanedHTML = cleanedHTML.split('<body').join('<body ' + metaData);
+      } else {
+        cleanedHTML = '<body ' + metaData + '>' + cleanedHTML + "</body>";
+        cleanedHTML = htmlTemplate.replace(/\<body[^>]*\>([^]*)\<\/body>/m, cleanedHTML);
       }
       // console.log('Content before saving: ' + cleanedHTML);
       return resolve(cleanedHTML);
